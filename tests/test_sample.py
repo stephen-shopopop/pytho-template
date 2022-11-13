426b3537a266a1d5317ab7f5bd9cf0492fd3f19c
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-11-13 10:26:27 +0100

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,14 @@
 # content of test_sample.py
 def inc(x):
+    """
+    increment
+    """
     return x + 1
 
 
 def test_answer():
+    """
+    >>> inc(3)
+    >>> 4
+    """
     assert inc(3) == 4
